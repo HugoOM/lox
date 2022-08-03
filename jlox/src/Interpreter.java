61263ef18ec10dd042e36d5a1700f491c4301a2d
Hugo-Olivier Monette <hugoo.monettev4si972@gmail.com>
2022-08-02 22:16:11 -0400

    Working Basic Class System

--- jlox/src/Interpreter.java
+++ jlox/src/Interpreter.java
@@ -61,6 +61,26 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	}
 
 	@Override
+	public Object visitSetExpr(Expr.Set expr) {
+		Object object = evaluate(expr.object);
+
+		if (!(object instanceof LoxInstance)) {
+			throw new RuntimeError(expr.name, "Only instances have fields.");
+		}
+
+		Object value = evaluate(expr.value);
+
+		((LoxInstance)object).set(expr.name, value);
+
+		return value;
+	}
+
+	@Override
+	public Object visitThisExpr(Expr.This expr) {
+		return lookupVariable(expr.keyword, expr);
+	}
+
+	@Override
 	public Object visitGroupingExpr(Expr.Grouping expr) {
 		return evaluate(expr.expression);
 	}
@@ -182,6 +202,17 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 		return function.call(this, arguments);
 	}
 
+	@Override
+	public Object visitGetExpr(Expr.Get expr) {
+		Object object = evaluate(expr.object);
+
+		if (!(object instanceof LoxInstance)) {
+			throw new RuntimeError(expr.name, "Only instances have properties.");
+		}
+
+		return ((LoxInstance) object).get(expr.name);
+	}
+
 	// Because of right-associativity, I need to evaluate both branches, 
 	// 	with elseBranch first... Right?
 	@Override
@@ -240,6 +271,23 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	}
 
 	@Override
+	public Void visitClassStmt(Stmt.Class stmt) {
+		environment.define(stmt.name.lexeme, null);
+		// LoxClass klass = new LoxClass(stmt.name.lexeme);
+
+		Map<String, LoxFunction> methods = new HashMap<>();
+
+		for (Stmt.Function method : stmt.methods) {
+			LoxFunction function = new LoxFunction(method, environment, method.name.lexeme.equals("init"));
+			methods.put(method.name.lexeme, function);
+		}
+		
+		environment.assign(stmt.name, new LoxClass(stmt.name.lexeme, methods));
+
+		return null;
+	}
+
+	@Override
 	public Void visitExpressionStmt(Stmt.Expression stmt) {
 		evaluate(stmt.expression);
 		return null;
@@ -247,7 +295,7 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
 	@Override
 	public Void visitFunctionStmt(Stmt.Function stmt) {
-		LoxFunction function = new LoxFunction(stmt, environment);
+		LoxFunction function = new LoxFunction(stmt, environment, false);
 		environment.define(stmt.name.lexeme, function);
 		return null;
 	}
