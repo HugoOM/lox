c8f2d1e7083a77d65793c3699e83b2a793ced25e
Hugo-Olivier Monette <hugoo.monettev4si972@gmail.com>
2022-08-24 23:21:02 -0400

    Working Basic Bytecode VM

--- clox/src/chunk.h
+++ clox/src/chunk.h
@@ -4,16 +4,28 @@
 #include "common.h"
 #include "value.h"
 
+// Value-less Enum's enumerations are represented as 0-indexed uint8_t
 typedef enum {
 	OP_CONSTANT,
+	OP_ADD,
+	OP_SUBTRACT,
+	OP_MULTIPLY,
+	OP_DIVIDE,
+	OP_NEGATE,
 	OP_RETURN,
 } OpCode;
 
 typedef struct {
 	int count;
 	int capacity;
+	
+	// Pointer to start of uint8_t dynamic array (OpCodes)
 	uint8_t* code;
+
+	// Pointer to start of int dynamic array (Line numbers)
 	int* lines;
+
+	// Actual dynamic array of constants
 	ValueArray constants;
 } Chunk;
 
