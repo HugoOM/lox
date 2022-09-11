67ed85ffa9b254a85cbee0d983c7f9b7bcf75026
Hugo-Olivier Monette <hugoo.monettev4si972@gmail.com>
2022-09-10 20:34:16 -0400

    All types implemented to clox, except strings.

--- clox/src/value.h
+++ clox/src/value.h
@@ -3,7 +3,32 @@
 
 #include "common.h"
 
-typedef double Value;
+typedef enum {
+	VAL_BOOL,
+	VAL_NIL,
+	VAL_NUMBER,
+} ValueType;
+
+typedef struct {
+	ValueType type;
+	union {
+		bool boolean;
+		double number;
+	// Name of the union's field. Would read i.e. { type: VAL_BOOL, as: true } 
+	} as;
+} Value;
+
+#define IS_BOOL(value) ((value).type == VAL_BOOL)
+#define IS_NIL(value) ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+
+// These use the tagged union's dotted syntax to specify the union's type
+#define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 
 typedef struct {
 	int capacity;
@@ -11,6 +36,7 @@ typedef struct {
 	Value* values;
 } ValueArray;
 
+bool valuesEqual(Value a, Value b);
 void initValueArray(ValueArray* array);
 void writeValueArray(ValueArray* array, Value value);
 void freeValueArray(ValueArray* array);
